5f3a02f3bdecb3959b6b45ba4c77828d57194600
Will Jones <willjones2010@gmail.com>
2019-03-21 19:46:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,8 @@ end
 
 def batch_badge_creator(array)
   new_array = []
- array.each do |name| new_array.push("Hello, my name is #{name}.")
+ array.each do |name| 
+   new_array.push("Hello, my name is #{name}.")
  return new_array
 end
 end
