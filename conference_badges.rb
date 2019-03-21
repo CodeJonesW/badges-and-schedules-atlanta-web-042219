f2bf4ecd1c1df5aa94237584438b3d1904efbeab
Will Jones <willjones2010@gmail.com>
2019-03-21 19:33:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(attendees)
   array.each do |name|
     puts "Hello, my name is #{name}."
 end
